8cc88fddd5139127d9470f2d3482ca0f6e1ec6e2
Daniel-Esteban <dani.este.bri@gmail.com>
2019-11-19 17:35:47 +0100

    Add missing modules to control_del_teclado

--- control_del_teclado.vhd
+++ control_del_teclado.vhd
@@ -36,11 +36,39 @@ architecture arch1 of control_del_teclado is
         ) ;
     end component ; 
 
+    component tec_control_tecla is
+        port (
+            clk        : in std_logic;
+            reset_l    : in std_logic;
+            fin        : in std_logic;
+            codigo_in  : in std_logic_vector(7 downto 0);
+            codigo_out : out std_logic_vector(7 downto 0);
+            make       : out std_logic;
+            break      : out std_logic
+        ) ;
+    end component ; 
+
+    component tec_control_pulsado is
+        port (
+          clk     : in std_logic;
+          reset_l : in std_logic;
+          make    : in std_logic;
+          break   : in std_logic;
+          codigo  : in std_logic_vector(7 downto 0);
+          tecla   : out std_logic_vector(3 downto 0);
+          pulsado : out std_logic
+        ) ;
+      end component ; 
+
     signal ps2_clk_limpio: std_logic;
     signal ps2_dat_limpio: std_logic;
 
     signal fin: std_logic;
     signal codigo: std_logic_vector(7 downto 0) ;
+    signal codigo_out: std_logic_vector(7 downto 0);
+    signal make  : std_logic;
+    signal break : std_logic;
+    
 begin
 
     limp_sen_comp : tec_limpiar_senales
@@ -62,4 +90,28 @@ begin
         fin     => fin,
         codigo  => codigo
     ) ;
+
+    control_tecla_comp : tec_control_tecla
+        port map (
+            clk        => clk,
+            reset_l    => reset_l,
+            fin        => fin,
+            codigo_in  => codigo,
+            codigo_out => codigo_out,
+            make       => make,
+            break      => break
+        ) ;
+
+    control_pulsado_comp : tec_control_pulsado
+        port (
+            clk     => clk,
+            reset_l => reset_l,
+            make    => make,
+            break   => break,
+            codigo  => codigo_out,
+            tecla   => tecla,
+            pulsado => pulsado
+        ) ;
+
+        
 end architecture ;
